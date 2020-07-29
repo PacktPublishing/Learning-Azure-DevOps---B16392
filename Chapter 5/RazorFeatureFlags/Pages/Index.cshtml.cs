9d68dd002424887aed3d28c44e1e540b3e9626b6
Sjoukje Zaal <sjoukje.zaal@sjoukjezaal.com>
2020-07-29 17:06:44 +0200

    Added code files for ch 5 + 7

---
+++ Chapter 5/RazorFeatureFlags/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace RazorFeatureFlags.Pages
+{
+    public class IndexModel : PageModel
+    {
+     private readonly IFeatureManager _featureManager;
+     public IndexModel(IFeatureManager featureManager)
+     {
+        _featureManager = featureManager;
+     }
+     public static string WelcomeMessage { get; set; }
+
+     public async Task OnGetAsync() 
+     {
+        WelcomeMessage = await _featureManager.IsEnabledAsync("ChangeBanner") ? "Welcome to Learn Azure DevOps" : "Welcome";
+     }
+    }
+}
